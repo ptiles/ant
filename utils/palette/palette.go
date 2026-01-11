92dd11baa8e6c0cb16c2da81f380d66113f660bf
yura.hm <yura.hm@gmail.com>
2026-01-11 10:06:22 +0200

    More customizable palettes

--- utils/palette/palette.go
+++ utils/palette/palette.go
@@ -9,34 +9,43 @@ import (
 type Palette []color.RGBA
 
 func GetPaletteRainbow(steps int) Palette {
+	return GetPaletteRainbowCL(steps, .95, .95)
+}
+
+func GetPaletteRainbowCL(steps int, c, l float64) Palette {
 	var palette = make(Palette, steps)
 
 	for si := range steps {
 		h := float64((si*360/steps + 180) % 360)
-		c := .95
-		l := .95
-		r, g, b := colorful.Hcl(h, c, l).Clamped().RGB255()
-		palette[si] = color.RGBA{R: r, G: g, B: b, A: 0xff}
+		palette[si] = hcl2rgb(h, c, l)
 	}
 
 	return palette
 }
 
 func GetPaletteMonochromatic(steps int, seedString string) Palette {
-	var palette = make(Palette, steps)
-
 	rng := rngFromString(seedString)
 	h := float64(rng.IntN(360))
 
+	return GetPaletteHCL(steps, h, .95, .65)
+}
+
+func GetPaletteHCL(steps int, h, c_, l_ float64) Palette {
+	var palette = make(Palette, steps)
+
 	for si := range steps {
 		sf := float64(si) / float64(steps)
-		c := .95 - 0.3*sf
-		l := .65 + 0.3*sf
-		r, g, b := colorful.Hcl(h, c, l).Clamped().RGB255()
-		palette[si] = color.RGBA{R: r, G: g, B: b, A: 0xff}
+		c := c_ - 0.3*sf
+		l := l_ + 0.3*sf
+		palette[si] = hcl2rgb(h, c, l)
 	}
 
 	return palette
+}
+
+func hcl2rgb(h, c, l float64) color.RGBA {
+	r, g, b := colorful.Hcl(h, c, l).Clamped().RGB255()
+	return color.RGBA{R: r, G: g, B: b, A: 0xff}
 }
 
 func rngFromString(seedString string) *rand.Rand {
